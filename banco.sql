f012b1cd320fa300ae01fa3bcc84e049ad17ad7c
Jaqueline Bauleo <44074113+jaquebauleo@users.noreply.github.com>
2021-03-15 15:59:04 -0300

    Add files via upload

---
+++ banco.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS eveniment;
+useCREATE DATABASE eveniment DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE eveniment;
+
+
+create table user(
+    idUser integer primary key not null AUTO_INCREMENT,
+    name varchar(10) not null,
+    password varchar(30) not null,
+    email varchar(30) not null,
+    isAdmin boolean not null,
+    registration integer not null,
+    ocupation varchar(20) not null,
+    dtUpdate date not null
+);
+
+create table event(
+    idEvent integer primary key not null AUTO_INCREMENT,
+    name varchar (20) not null,
+    cdConfirm varchar (20) not null,
+    description varchar(200) not null,
+    dtInit date not null,
+    dtEnd date not null,
+    dtUpdate date not null,
+    image varchar (40) not null,
+    idUs integer not null,
+    foreign key (idUs) references user(idUser)
+);
+
+INSERT INTO `user` (`idUser`, `name`, `password`, `email`, `isAdmin`, `registration`, `ocupation`, `dtUpdate` ) VALUES
+(00, 'Jaqueline', 'Pgp@13', 'false', 'Estudante')
+);
