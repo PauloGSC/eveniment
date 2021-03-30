27fb1342dbe5a05da11046f7067cd4f6572abe90
herrera <henriqueherrera9@gmail.com>
2021-03-30 19:12:34 -0300

    :sparkles: feat: created methods in eventSrv for find all events and create events

--- server/app/services/eventSrv.js
+++ server/app/services/eventSrv.js
@@ -2,21 +2,31 @@
 
 class EventSrv {
 
-	constructor(repository) {
+  constructor(repository) {
     this.repository = repository;
   }
 
   async create(model) {
     try {
       const columns = Object.keys(model);
-      const data = columns.map(key => typeof model[key] == 'string' ? `"${model[key]}"` : model[key] );
-      
-      const query = `INSERT INTO event (${columns.join(',')})` + 
-        ` VALUES (` + data.join(',') +  `)`;
+      const data = columns.map(key => typeof model[key] == 'string' ? `"${model[key]}"` : model[key]);
+
+      const query = `INSERT INTO event (${columns.join(',')})` +
+        ` VALUES (` + data.join(',') + `)`;
       const [response] = await this.repository.execute(query);
 
       return response.insertId;
-    } catch(err) {
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async findAll(query) {
+    try {
+      const [_models] = await this.repository.execute('SELECT * FROM event');
+
+      return _models;
+    } catch (err) {
       return err;
     }
   }
